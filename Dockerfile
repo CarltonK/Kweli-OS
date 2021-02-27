6380306e29e4e6cf040b5026070ff3fc5e2f5d4c
Mark <mcarlton33@gmail.com>
2021-02-27 17:01:21 +0300

    API liveness check

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,10 @@ FROM node:12-alpine
 
 WORKDIR /app
 
+# add `/app/node_modules/.bin` to $PATH
+ENV PATH /app/node_modules/.bin:$PATH
+
+# install and cache app dependencies
 COPY package*.json ./
 
 RUN npm i
@@ -11,5 +15,7 @@ COPY . /app
 RUN npm run lint
 
 RUN npm run build
+
+EXPOSE 8080
 
 CMD [ "npm", "run", "serve" ]
--- src/index.ts
+++ src/index.ts
@@ -11,15 +11,15 @@ main.use('/api/',app);
 main.use( express.json() );
 
 // PORT
-const port = process.env.PORT || 8080
+const port = process.env.PORT;
 
 app.get('/', (req, res) => {
     res.status(200).send({
         status: true,
-        detail: 'Kweli OS is live!'
-    })
-})
+        detail: 'Kweli OS is live!',
+    });
+});
 
 app.listen(port, () => {
-    console.log(`Server started at http://localhost:${port}`)
-})
+    console.log(`Server started at http://localhost:${port}`);
+});
