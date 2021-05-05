9d6be4ea5027c88b337e80cb7c324a18c208b68a
Mark Carlton <mcarlton33@gmail.com>
2021-05-05 20:58:17 +0200

    init sequelize

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
 import App from './app';
 import PingController from './controllers/ping';
 import BaseController from './controllers/base';
+import DBConfig from './config/db';
 
 const port = process.env.PORT || 8080;
+
+new DBConfig();
  
 const app = new App(
   [
